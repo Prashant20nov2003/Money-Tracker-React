b76166bb20cc666c588208da74ece6d60c8da3b7
Prashant20nov2003 <prashanttbhardwajj@gmail.com>
2023-11-07 09:56:39 +0530

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,34 @@
+// rrd imports
+import { Form, NavLink } from "react-router-dom";
+
+// library
+import { TrashIcon } from "@heroicons/react/24/solid";
+
+// assets
+
+const Nav = ({ userName }) => {
+  return (
+    <nav>
+      <NavLink to="/" aria-label="Go to home">
+        <span>Money Expense Tracker</span>
+      </NavLink>
+      {userName && (
+        <Form
+          method="post"
+          action="logout"
+          onSubmit={(event) => {
+            if (!confirm("Delete user and all data?")) {
+              event.preventDefault();
+            }
+          }}
+        >
+          <button type="submit" className="btn btn--warning">
+            <span>Delete User</span>
+            <TrashIcon width={20} />
+          </button>
+        </Form>
+      )}
+    </nav>
+  );
+};
+export default Nav;
